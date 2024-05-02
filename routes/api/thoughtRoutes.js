3e9187352873e56bef6644916fa16e799477ff4d
HeidiHarmony <tenibrae@gmail.com>
2024-05-02 16:22:54 -0400

    Initial commit

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+// ADAPT! ADAPT! ADAPT!
+
+const router = require('express').Router();
+const {
+  getCourses,
+  getSingleCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../../controllers/courseController.js');
+
+// /api/courses
+router.route('/').get(getCourses).post(createCourse);
+
+// /api/courses/:courseId
+router
+  .route('/:courseId')
+  .get(getSingleCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
+
+module.exports = router;
